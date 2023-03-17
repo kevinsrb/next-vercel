2420d26e8b999014a8a38f6a2037a755c65106e0
KEVINSRB <kevinsrb.1999@gmail.com>
2023-03-17 11:57:55 -0500

    Se arregla pronlemas con los CHILDREN

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next'
+import { AppProps } from 'next/app';
 
 import '../styles/globals.css'
-import { AppProps } from 'next/app';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: JSX.Element ) =>  JSX.Element
@@ -11,9 +11,17 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
-  const getLayout = Component.getLayout || ((page) => page);
-  //return <Component {...pageProps} />
-  return getLayout(<Component {...pageProps} />)
+function MyApp({ Component, pageProps }: AppPropsWithLayout ) {
+
+  const getLayout = Component.getLayout || ((page) => page );
+
+  // return (
+  //   <>
+  //     <Component {...pageProps} />
+  //   </>
+  // )
+  return getLayout( <Component {...pageProps} /> )
 }
+
+export default MyApp
